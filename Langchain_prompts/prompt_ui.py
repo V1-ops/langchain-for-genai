e1d488463dd13484eaada5ff6d9c2594ca60800d
V1-ops <vanshdeepsinghlamba656@gmail.com>
2026-01-08 16:41:20 +0530

    Static prompt example on streamlit

---
+++ Langchain_prompts/prompt_ui.py
@@ -0,0 +1,20 @@
+from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st 
+load_dotenv()
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-72B-Instruct",
+    task="text-generation",
+    temperature=0.7,
+    max_new_tokens=50,
+)
+model = ChatHuggingFace(llm=llm)
+
+st.header('Research Assistant')
+
+user_input=st.text_input('Enter your Query...')
+if st.button('Summarize'):
+    result=model.invoke(user_input)
+    st.write(result.content)
+# Static Prompt example 
