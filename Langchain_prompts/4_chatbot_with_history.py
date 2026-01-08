c918a4f91347180b5966a80b642f3a0950e33b33
V1-ops <vanshdeepsinghlamba656@gmail.com>
2026-01-08 23:49:53 +0530

    updated example

--- Langchain_prompts/4_chatbot_with_history.py
+++ Langchain_prompts/4_chatbot_with_history.py
@@ -20,5 +20,6 @@ while True:
     chat_history.append(result.content)
     print("Ai:",result.content)
 print(chat_history )
-
+# example of a sample conversation stored in chat_history
+#['which is bigger 2 or 3 ', 'The number 3 is bigger than the number 2.', 'then multiply the bigger with 10 ', 'Since 3 is the bigger number, you would multiply it by 10:\n\n\\[ 3 \\times 10 = 30 \\]\n\nSo, the result is 30.', 'exit']
 # Now this chatbot can remeber the previous conversation as we are passing the chat_history to the model.invoke() function, but a new problem rises here , the chat_history is just a list of strings ,where it is not clear which string is from user and which string is from AI . To solve this problem langchain introduced the concept of Messages like HumanMessage and AIMessage to clearly differentiate between user and AI messages in the chat history. This will be implemented in the next file.
